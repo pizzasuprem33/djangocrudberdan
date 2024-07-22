c776fa9a13bfdb41f5241830a0ff5a61007c0b78
Arnon Isaac Berdan <arnonisaacberdan005@gmail.com>
2024-07-22 14:49:15 +0800

    first commit

---
+++ berdanapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BerdanappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'berdanapp'
---
+++ berdanapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password
+
+class Gender(models.Model):
+    class Meta:
+        db_table = 'tbl_genders'
+
+    gender_id = models.BigAutoField(primary_key=True)
+    gender = models.CharField(blank=False, max_length=55)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+class User(models.Model):
+    class Meta:
+        db_table = 'tbl_users'
+        
+    user_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(blank=False, max_length=55)
+    middle_name = models.CharField(blank=True, max_length=55)
+    last_name = models.CharField(blank=False, max_length=55)
+    age = models.IntegerField(blank=False)
+    gender_id = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    birthdate = models.DateField(blank=False)
+    username = models.CharField(blank=False, max_length=55)
+    password = models.CharField(blank=False, max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def save(self):
+        if not self.pk:
+            self.password = make_password(self.password)
+        
+        super(User, self).save()
